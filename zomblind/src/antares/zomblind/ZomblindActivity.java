ade82a2b323640d96f67b3c333ebf45d4e332d03
Antonio Fernández Ares <antares.es@gmail.com>
2013-01-11 07:38:47 +0100

    Cambiado el sensor -magnético comentado

--- zomblind/src/antares/zomblind/ZomblindActivity.java
+++ zomblind/src/antares/zomblind/ZomblindActivity.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -39,28 +41,19 @@ import android.view.View;
 
 }*/
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Bundle;
-import android.view.View;
  
-public class ZomblindActivity extends Activity implements SensorEventListener {
+public class ZomblindActivity extends Activity /*implements SensorEventListener*/ {
   
   Boolean debug = true;
   Boolean calibrado = false;
-  Float azimut;  // View to draw a compass
-  Float original_azimut;
+  Float azimut;  // Orientación
+  Float original_azimut; //Orientación de calibrado
   float position_touch_x = 50;
   float position_touch_y = 50;
   
   String action = "";
+  
+  entorno _entorno = null;
  
   public class CustomDrawableView extends View {
     Paint paint = new Paint();
@@ -98,7 +91,13 @@ public class ZomblindActivity extends Activity implements SensorEventListener {
 		canvas.drawText(Float.toString(azimut - original_azimut), 10, delay_text*11, paint);
 		
 		canvas.drawText("Posición de mira (entera)",10, delay_text*12, paint);
-		canvas.drawText(Double.toString(Math.ceil(azimut - original_azimut)), 10, delay_text*13, paint);
+		String aux = "";
+		if((azimut - original_azimut)<0.5){
+			aux = "izquierda";
+		}else if((azimut - original_azimut)>0.5){
+			
+		}
+		canvas.drawText(aux, 10, delay_text*13, paint);
     	}
       
     }
@@ -127,53 +126,91 @@ public class ZomblindActivity extends Activity implements SensorEventListener {
   }
  
   CustomDrawableView mCustomDrawableView;
-  private SensorManager mSensorManager;
-  Sensor accelerometer;
-  Sensor magnetometer;
+//  private SensorManager mSensorManager;
+ // Sensor accelerometer;
+//  Sensor magnetometer;
  
+  private static SensorManager sensorService;
+  private Sensor sensor;
+  
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    _entorno = new entorno(ZomblindActivity.this);
     mCustomDrawableView = new CustomDrawableView(this);
     setContentView(mCustomDrawableView);    // Register the sensor listeners
-    mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-    accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-  }
- 
-  protected void onResume() {
-    super.onResume();
-    mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-    mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
-  }
- 
-  protected void onPause() {
-    super.onPause();
-    mSensorManager.unregisterListener(this);
-  }
- 
-  public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
- 
-  float[] mGravity;
-  float[] mGeomagnetic;
-  public void onSensorChanged(SensorEvent event) {
-	  synchronized (this) {
-    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-      mGravity = event.values;
-    if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-      mGeomagnetic = event.values;
-    if (mGravity != null && mGeomagnetic != null) {
-      float R[] = new float[9];
-      float I[] = new float[9];
-      boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
-      if (success) {
-        float orientation[] = new float[3];
-        SensorManager.getOrientation(R, orientation);
-        if(original_azimut==null){ original_azimut = orientation[0]; // orientation contains: azimut, pitch and roll}
-        }else {azimut = orientation[0];}
-        
-      }
+//    mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+//    accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//    magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+      sensorService = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+      sensor = sensorService.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+    if (sensor != null) {
+      sensorService.registerListener(mySensorEventListener, sensor,
+          SensorManager.SENSOR_DELAY_NORMAL);
+      Log.i("Compass MainActivity", "Registerered for ORIENTATION Sensor");
+
+    } else {
+      Log.e("Compass MainActivity", "Registerered for ORIENTATION Sensor");
+      //Toast.makeText(this, "ORIENTATION Sensor not found",
+      //    Toast.LENGTH_LONG).show();
+      finish();
     }
-    mCustomDrawableView.invalidate();
-	  }
+    
   }
+  
+  private SensorEventListener mySensorEventListener = new SensorEventListener() {
+
+	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    }
+
+	    public void onSensorChanged(SensorEvent event) {
+	      // angle between the magnetic north directio
+	      // 0=North, 90=East, 180=South, 270=West
+	      // azimut = event.values[0];
+	      //compassView.updateData(azimuth);
+	      
+	      if(original_azimut==null){ original_azimut = event.values[0]; // orientation contains: azimut, pitch and roll}
+        }else {azimut = event.values[0];}
+	      mCustomDrawableView.invalidate();
+	    }
+	    
+	    
+	  };
+  
+ 
+//  protected void onResume() {
+//    super.onResume();
+//    mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+//    mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+//  }
+// 
+//  protected void onPause() {
+//    super.onPause();
+//    mSensorManager.unregisterListener(this);
+//  }
+// 
+//  public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
+// 
+//  float[] mGravity;
+//  float[] mGeomagnetic;
+//  public void onSensorChanged(SensorEvent event) {
+//	  synchronized (this) {
+//    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+//      mGravity = event.values;
+//    if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+//      mGeomagnetic = event.values;
+//    if (mGravity != null && mGeomagnetic != null) {
+//      float R[] = new float[9];
+//      float I[] = new float[9];
+//      boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+//      if (success) {
+//        float orientation[] = new float[3];
+//        SensorManager.getOrientation(R, orientation);
+//        if(original_azimut==null){ original_azimut = orientation[0]; // orientation contains: azimut, pitch and roll}
+//        }else {azimut = orientation[0];}
+//        
+//      }
+//    }
+//    mCustomDrawableView.invalidate();
+//	  }
+//  }
 }
